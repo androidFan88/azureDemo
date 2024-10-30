0dda85c8eb89ed1a0f6d8c0bb26f06f63a1e927e
androidFan88 <agustafson97@gmail.com>
2024-10-30 12:26:25 -0400

    we have added a subfolder and a new file

---
+++ stuff/page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our test website</title>
+</head>
+<body>
+    <h1>This is a test website</h1>
+    <?php 
+        echo "hello!"
+    ?>
+</body>
+</html>
